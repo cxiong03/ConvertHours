9d1c77dc2d9b3b2b6e28c128223846034908263f
Chang <cnxiong03@gmail.com>
2021-01-15 16:58:22 -0500

    version 2 done

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -1,12 +1,14 @@
 package com.codewithchang;
 
+import jdk.jshell.execution.Util;
+
 public class Main {
 
     public static void main(String[] args) {
         // Version 1
 	    Utility.convertHours(8,0,5);
 	    Utility.convertHours(21, 0, 5);
-	    Utility.convertHours(21, 0, -36);
+	    Utility.convertHours(21, 0, 36);
 
 	    // Version 2
         Utility.convertHours(21, 0, -5);
--- src/com/codewithchang/Utility.java
+++ src/com/codewithchang/Utility.java
@@ -5,16 +5,24 @@ public class Utility {
     // Version 1
     public static void convertHours(int hours, int minutes, int hoursPassed) {
 
+        int timeOfHour = (hours + hoursPassed) % 24;
+
         String minString = ":0";
         if (minutes < 10) {
             minString += minutes;
         } else {
             minString = ":" + minutes;
         }
-        int timeOfHour = (hours + hoursPassed) % 24;
+
+        if (hoursPassed < 0) {
+            int negativeTime = hours + hoursPassed;
+        }
+
 
         System.out.println("The time is " + timeOfHour + minString);
     }
+
+    // Version 2 allow negative time
 
     // Version 3
     public static void convertMinutes(int hours, int minutes, int minutesPassed) {
